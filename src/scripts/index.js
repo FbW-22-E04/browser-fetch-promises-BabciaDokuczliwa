cb190d64442c604960a96b861713d0ea72dc6c13
BabciaDokuczliwa <oblaczynska@gmail.com>
2022-09-28 14:21:07 +0200

    modul/promise

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,13 @@
 // The following line makes sure your styles are included in the project. Don't remove this.
-import '../styles/main.scss';
+import "../styles/main.scss";
 // Import any additional modules you want to include below \/
 
-
 // \/ All of your javascript should go here \/
-
+new Promise(function (resolve, reject) {
+  const modal = document.getElementsByClassName("modal")[0];
+  setTimeout(() => resolve((modal.style.display = "block")), 6000);
+});
+const close = document.getElementsByClassName("close")[0];
+close.addEventListener("click", () => {
+  document.getElementsByClassName("modal")[0].style.display = "none";
+});
